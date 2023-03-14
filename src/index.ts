e7f3b02d4b01b914fe400b0b87a3b97f8b27e6b0
Hendrik Mennen <hmennen@protop-solutions.com>
2023-03-14 16:53:02 +0100

    progress

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import {execSync} from "child_process";
 
 const app = express();
 
-app.get('/', (req: any, res: any) => { res.send('Hello World') });
+app.get('/', (req: any, res: any) => { 
+    let t = execSync("yosys");
+    res.send(t.toString());
+});
 
 app.listen(3000, () => { console.log('Server is up!') })
